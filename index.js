db0a539feb98c999e33ef8a92093550d61a3e04b
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2021-08-12 19:52:42 -0400

    done

--- index.js
+++ index.js
@@ -139,8 +139,7 @@ Use the getLastReview function below to do the following:
 
 
 function getLastReview(array) {
-  array = array.length - 1;
-  return array
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`
 }
 
 
